8bc025f9c5e2139b65dade7f84a0aae899b440aa
Fernando Murua <Fernando.Murua@swissmedical.com.ar>
2022-09-05 16:00:08 -0300

    desafio clase05

--- src/componentes/Contenido.jsx
+++ src/componentes/Contenido.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import foto1 from "./img/material1.jpg";
+import ItemCount from "./ItemCount";
 
 const Productos = () => {
     let titulo = "Azulejo";
@@ -7,7 +8,10 @@ const Productos = () => {
     let modelo = "color-01";
     let precio = 200;
     let detalle = "Tamaño: 15x15cm Color: Aleatorio";
-    const producto = {titulo:titulo, marca:marca, modelo:modelo, precio:precio, detalle:detalle}
+    let stock = 5;
+    let inicial = 1;
+    let onAdd = 0;
+    const producto = {titulo:titulo, marca:marca, modelo:modelo, precio:precio, detalle:detalle, stock:stock, inicial:inicial, onAdd:onAdd}
     return(
         <div className="card mx-auto mb-3" style={{width: 300}}>
                 <img src={foto1} className="card-img-top" alt={producto.titulo}/>
@@ -17,7 +21,9 @@ const Productos = () => {
                     <p>Modelo: {producto.modelo}</p>
                     <p>Precio: ${producto.precio}</p>
                     <p className="card-text">Detalle:{producto.detalle}</p>
+                    <ItemCount stock = {producto.stock} inicial = {producto.inicial} onAdd = {producto.onAdd}/>                    
                 </div>
+                
                 
         </div>
     )
---
+++ src/componentes/ItemCount.jsx
@@ -0,0 +1,45 @@
+
+import React from "react";
+import { useState } from "react";
+
+const ItemCount = (prop) => {
+    const [cantidad, setCantidad] = useState(prop.inicial);
+    const [itemStock, setItemStock] = useState(prop.stock);
+    const [itemOnAdd, setItemOnAdd] = useState(prop.onAdd);
+
+    const decrementarCantidad = (valor) => {
+        if(valor > 0){
+            setCantidad(valor);
+        }
+    }
+    const incrementarCantidad = (valor) => {
+        if(valor <= itemStock){
+            setCantidad(valor);
+        }
+    }
+    const agregarProducto = () => {
+        if(cantidad <= itemStock){
+            setItemStock(itemStock - cantidad);
+            setItemOnAdd(itemOnAdd + cantidad);
+        }
+        
+
+    }
+
+    return(
+        <div>
+           <div className="input-group mb-3">
+                <input type="button" className="btn btn-info" value= "-" onClick={() => {decrementarCantidad(cantidad - 1)}}/>
+                <input type="text" className="form-control col-2" aria-label="Amount (to the nearest dollar)" value={cantidad}/>
+                <input type="button" className="btn btn-info" value= "+" onClick={() => {incrementarCantidad(cantidad + 1)}}/>
+            </div>
+            <div class="d-grid gap-2">
+                <button className="btn btn-info" type="button" onClick={() => {agregarProducto()}}>Agregar</button>
+            </div>
+            <p>Productos seleccionados {itemOnAdd}</p>
+        </div>
+
+    )
+
+} 
+export default ItemCount;
