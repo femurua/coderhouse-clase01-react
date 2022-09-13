0a337ecbc02b4d52fdf2485a57c84d3146c7865a
Fernando Murua <Fernando.Murua@swissmedical.com.ar>
2022-09-12 22:18:42 -0300

    desafio clase07

--- src/componentes/Item.jsx
+++ src/componentes/Item.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import ItemCount from "./ItemCount";
-const Item = (producto) => {
-    
+//import ItemCount from "./ItemCount";
+const Item = (item) => {
     return(
         <div className="card mx-auto mb-3" style={{width: 300}}>
-                <img src={producto.imagen} className="card-img-top p-2" alt={producto.nombre}/>
+                <img src={item.thumbnail} className="card-img-top p-2" alt={item.title}/>
                 <div className="card-body">
-                    <h5 className="card-title text-center">{producto.nombre}</h5>
-                    <p>Marca: {producto.marca}</p>
-                    <p>Modelo: {producto.modelo}</p>
-                    <p>Precio: ${producto.precio}</p>
-                    <p className="card-text">Detalle:{producto.detalle}</p>
-                    <ItemCount stock = {producto.cantidad} inicial = {producto.inicial} onAdd = {producto.onAdd}/>                    
-                </div>
-                
-                
+                    <h5 className="card-title text-center">{item.title}</h5>
+                    <p>Precio: ${item.price}</p>
+                    
+                                        
+                </div>    
         </div>
     )
 
--- src/componentes/ItemList.jsx
+++ src/componentes/ItemList.jsx
@@ -5,18 +5,7 @@ const ItemList = ({items}) => {
     return(
         <div className="row">
             {items.map(item => 
-                <Item  
-                key={item.id} 
-                categoria={item.categoria} 
-                nombre={item.nombre} 
-                marca={item.marca} 
-                modelo={item.modelo} 
-                precio={item.precio} 
-                cantidad={item.cantidad} 
-                inicial={item.inicial} 
-                onAdd={item.onAdd} 
-                detalle={item.detalle} 
-                imagen={item.imagen} />)}
+                <Item item={item} />)}
         </div>
     )
 
